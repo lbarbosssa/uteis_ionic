f69a2d2c1336479705600354eb2a4ae91c6dbd46
Lucas Barbosa <barbosa.lucas1996@gmail.com>
2018-12-05 21:33:43 -0200

    Accordion Funcionando

--- Accordion/src/app/app.module.ts
+++ Accordion/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AccordionComponent } from '../components/accordion/accordion';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AccordionComponent
   ],
   imports: [
     BrowserModule,
